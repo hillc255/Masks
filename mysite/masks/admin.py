df914f502947ca959df580c950575d6c65be9c6f
Claudia Hill <hillc255@yahoo.com>
2020-05-09 08:21:12 -0400

    update admin resource link

--- mysite/masks/admin.py
+++ mysite/masks/admin.py
@@ -1,10 +1,28 @@
 from django.contrib import admin
 from .models import Title
+from django.utils.html import format_html
+from django.utils.safestring import mark_safe
+
 
 # Register your models here.
 
 class TitleAdmin(admin.ModelAdmin):
-    list_display = ('title', 'materials', 'link', 'name', 'image', 'image_tag',)
-    readonly_fields = ('image_tag',)
+    list_display = ['title', 'materials', 'resource_link', 'update', 'image', 'image_tag',]
+    readonly_fields = ('update', 'image_tag',)
 
+
+    #def show_link(self, obj):
+        #return '<a href="%s">Source Link</a>' % obj.get_absolute_url()
+    #show_link.allow_tags = True
+
+        #url = show_url
+        #return mark_s<a href='%s'>Source Link</a>" % obj.show_url)
+    
+   # show_url.allow_tags = True
+
+        #if obj.show_url:
+            #return "<a href='%s'>Source Link</a>" % obj.show_url
+        #else:
+            #return ''
+                
 admin.site.register(Title, TitleAdmin)
--- mysite/masks/models.py
+++ mysite/masks/models.py
@@ -1,5 +1,5 @@
 import os.path
-import datetime
+from datetime import datetime
 
 from django.db import models
 from django.conf import settings
@@ -11,18 +11,15 @@ from django.utils.safestring import mark_safe
 class Title(models.Model):
     title = models.CharField(max_length=300)
     materials = models.CharField(max_length=800,blank=True)
-    link = models.URLField(
-            max_length=150,
-            db_index=True,
-            unique=True,
-            blank=True
-    )
-    name = models.CharField(max_length=50,blank=True)
+    resource = models.URLField(max_length=150,blank=True)
+    update = models.DateTimeField(auto_now_add=True)
     image = models.ImageField(upload_to='images/', blank=True)
     pub_date = models.DateTimeField('date published')
+   
     
     def __str__(self):
         return self.title
+    
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
    
@@ -35,14 +32,23 @@ class Title(models.Model):
     def image_tag(self):
         return mark_safe('<img src="{}" width="80" height="80" />'.format(self.url()) )
         #return mark_safe('<img src="{http://127.0.0.1:8000/media/images/tapping.jpg}" width="100 height="100" />')
-    image_tag.short_description = 'Image'
+    image_tag.short_description = 'Thumbnail'
 
-    def __unicode__(self):
-        return self.title
+    def resource_link(self):
+        return mark_safe('<a href="{}">Resource</a>'.format(self.resource) )
+        #return ('<a href="%s">Resource</a>' % self.resource)
+    resource_link.allow_tags = True
+
+    #def __unicode__(self):
+        #return self.title
+        #return self.show_url
+     
     
-    #def image(self):
-     #   from django.utils.html import escape
-      #  return u'<img src="%s" />' % self.image.url_123x123 
-        #escape("/media/images")
-    #image.short_description = 'Image'
+    #def get_absolute_url(self):
+        #return "/%i/" % self.id
+
+    #return mark_safe('<a href="%s">Resource<a/>' % url_link)
+        #return u'<a href="%s">Resource</a>' % (self.show_url)
+    #show_url.short_description = 'Link'
+    #show_url.allow_tags = True
     
